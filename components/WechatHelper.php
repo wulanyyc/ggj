4c470a60b27837c358b82be9c189c75c5cd2d0c3
yangyuncai <yyc@yunlaba.com>
2018-01-03 23:47:11 +0800

    test wechat

--- components/WechatHelper.php
+++ components/WechatHelper.php
@@ -158,7 +158,7 @@ class WechatHelper extends Component{
 
     public static function getCurrentUrl() {
         $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-        $url = $protocol . $_SERVER[HTTP_HOST] . $_SERVER[REQUEST_URI];
+        $url = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
         return $url;
     }
