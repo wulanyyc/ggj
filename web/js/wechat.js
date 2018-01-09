2a2dc1f4fcd41d10ebee8d37e16abb0a73f45092
yangyuncai <yyc@yunlaba.com>
2018-01-09 14:39:52 +0800

    test wx pay

--- web/js/wechat.js
+++ web/js/wechat.js
@@ -1,6 +1,6 @@
 if (document.getElementById('we_appid')) {
     wx.config({
-        //debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         appId: document.getElementById('we_appid').value, // 必填，公众号的唯一标识
         timestamp: document.getElementById('we_timestamp').value, // 必填，生成签名的时间戳
         nonceStr: document.getElementById('we_noncestr').value, // 必填，生成签名的随机串
