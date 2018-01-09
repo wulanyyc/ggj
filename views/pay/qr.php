75fa420173adb24d7c7b4aa26914135b98cbedb2
yangyuncai <yyc@yunlaba.com>
2018-01-09 21:30:49 +0800

    test wx pc pay

--- views/pay/qr.php
+++ views/pay/qr.php
@@ -29,7 +29,7 @@ MsaView::registerJsFile($this,'/js/pay/qr.js',
 
 <input type="hidden" value="<?=$url; ?>" id="wechat_url"></input>
 <div class="card" id="qrcontent" style="display: flex;flex-direction: column;justify-content: center;align-items: center;">
-    <p>微信扫码支付</p>
+    <p style="text-align: center;">微信扫码支付</p>
     <div id="qrcode" style="width:250px; height:250px;"></div>
     <a class="btn btn-outline-info btn-sm" style="margin-top: 10px;" href="/pay/?out_trade_no=<?=$out_trade_no ?>">支付完成后查看订单状态</a>
 </div>
--- web/js/pay/qr.js
+++ web/js/pay/qr.js
@@ -3,8 +3,8 @@ $(document).ready(function () {
 
     var qrcode = new QRCode(document.getElementById("qrcode"), {
         text : $("#wechat_url").val(),
-        width : 300,
-        height : 300,
+        width : 250,
+        height : 250,
         colorDark : "#000000",
         colorLight : "#ffffff",
         correctLevel : QRCode.CorrectLevel.H
