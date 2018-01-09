6da8df24880e17e648cbc2d141e70f262b68cead
yangyuncai <yyc@yunlaba.com>
2018-01-09 14:53:35 +0800

    test wx pay

--- web/js/order/pay.js
+++ web/js/order/pay.js
@@ -61,17 +61,18 @@ $(document).ready(function () {
                             // $.helper.alert('测试微信支付')
                             var wechat = $('#wechat').val();
                             if (wechat == 1) {
-                                wx.chooseWXPay({
-                                    timestamp: data.data.timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
-                                    nonceStr: data.data.nonceStr, // 支付签名随机串，不长于 32 位
-                                    package: data.data.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=\*\*\*）
-                                    signType: data.data.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
-                                    paySign: data.data.paySign, // 支付签名
-                                    success: function (res) {
-                                        // 支付成功后的回调函数
-                                        $.helper.alert('支付成功')
-                                    }
-                                });
+                                alert(data.data);
+                                // wx.chooseWXPay({
+                                //     timestamp: data.data.timeStamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+                                //     nonceStr: data.data.nonceStr, // 支付签名随机串，不长于 32 位
+                                //     package: data.data.package, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=\*\*\*）
+                                //     signType: data.data.signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+                                //     paySign: data.data.paySign, // 支付签名
+                                //     success: function (res) {
+                                //         // 支付成功后的回调函数
+                                //         $.helper.alert('支付成功')
+                                //     }
+                                // });
                             }
                         }
                     } else {
