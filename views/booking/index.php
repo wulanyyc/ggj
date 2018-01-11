e9e9d73fd353a66e59a48b58be92fd69c05acaf4
yangyuncai <yyc@yunlaba.com>
2018-01-12 02:49:45 +0800

    adjust home page

--- views/booking/index.php
+++ views/booking/index.php
@@ -18,7 +18,6 @@ SiteAsset::register($this);
 
   .label {
     display: inline-block;
-    width: 120px;
     color: #53a93f;
   }
 
@@ -55,7 +54,7 @@ SiteAsset::register($this);
         <div class="label">
           <i class="fa fa-shopping-cart" aria-hidden="true"></i>&nbsp;绝对新鲜：
         </div>
-        <div>
+        <div style="width:70%;">
           早上在水果市场进货，上午打包，下午快递发货（<span style="color:red;font-weight: bold;">隔日达</span>）。
         </div>
       </li>
@@ -64,7 +63,7 @@ SiteAsset::register($this);
         <div class="label">
           <i class="fa fa-shopping-cart" aria-hidden="true"></i>&nbsp;水果丰富：
         </div>
-        <div>
+        <div style="width:70%;">
           品类根据时令动态调整，只卖好吃又健康的水果，如有特殊需求请联系我们。
         </div>
       </li>
@@ -73,7 +72,7 @@ SiteAsset::register($this);
         <div class="label">
           <i class="fa fa-shopping-cart" aria-hidden="true"></i>&nbsp;价格实惠：
         </div>
-        <div>
+        <div style="width:70%;">
           低于市场价，并享受平台各种优惠。
         </div>
       </li>
@@ -82,7 +81,7 @@ SiteAsset::register($this);
         <div class="label">
           <i class="fa fa-shopping-cart" aria-hidden="true"></i>&nbsp;预约限制：
         </div>
-        <div>
+        <div style="width:70%;">
         为保证水果新鲜度，仅接受<span style="color:red;font-weight: bold;">成都</span>地区的订单，<span style="color:red;font-weight: bold;">周一、三、五</span>发货，二、四、六到货。如遇缺货无法完成订单，立即退款。
         </div>
       </li>
