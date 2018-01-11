e61779493c26b1c1e7693a579947250e1b4d266c
yangyuncai <yyc@yunlaba.com>
2018-01-12 01:24:30 +0800

    adjust home page

--- views/layouts/pc.php
+++ views/layouts/pc.php
@@ -16,7 +16,7 @@ SiteAsset::register($this);
 <html lang='<?= Yii::$app->language ?>'>
 <head>
     <meta charset='<?= Yii::$app->charset ?>'/>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <meta name='viewport' content='width=device-width, minimum-scale=1.0,maximum-scale=1.0,user-scalable=no'>
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
