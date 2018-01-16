37640699c0262aa665ab249e4fa08bc48b8a3051
yangyuncai <yyc@yunlaba.com>
2018-01-16 09:07:31 +0800

    test wechat

--- views/customer/index.php
+++ views/customer/index.php
@@ -125,8 +125,8 @@ MsaView::registerJsFile($this,'/js/customer/index.js',
 </a>
 
 <?php if (!$isWechat) { ?>
-<button type="button" class="btn btn-danger btn-sm" id="quit" style="width:80%;margin-left: 10%;margin-top: 6px;">退出</button>
+<button type="button" class="btn btn-danger" id="quit" style="width:80%;margin-left: 10%;margin-top: 6px;">退出</button>
 <?php } else { ?>
-<a class="btn btn-success btn-danger" href="/site" style="width:80%;margin-left: 10%;margin-top: 6px;text-decoration: none;">逛商城</a>
+<a class="btn btn-danger" href="/site" style="width:80%;margin-left: 10%;margin-top: 6px;text-decoration: none;">逛商城</a>
 <?php } ?>
 
