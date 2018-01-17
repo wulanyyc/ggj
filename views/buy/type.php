1a0a0de8638a03f6949a9e82e75260ab2860bf17
yangyuncai <yyc@yunlaba.com>
2018-01-17 11:50:21 +0800

    adjust type

--- views/buy/type.php
+++ views/buy/type.php
@@ -57,17 +57,24 @@ MsaView::registerJsFile($this,'/js/buy/type.js',
     border:none;
   }
 
+  #type_content {
+    display: table-cell;
+    vertical-align: middle;
+  }
+
   footer {
     display: none;
   }
 </style>
 
-<div class="card" id="type_content" style="display: -webkit-flex;align-items: -webkit-center;justify-content: -webkit-center;display: flex;align-items: center;justify-content: center;">
+<div class="card" id="type_content">
     <div class="card buy-type">
       <div id="rule" style="margin-top: 1%;font-size: 14px;width:100%;">
-        <div style="display: flex;flex-direction: row;justify-content: center;align-items: center;">
-          <img src="http://img.guoguojia.vip/img/booking.png" style="width: 90px;"/>
-          <ul style="width:60%">
+        <div style="display: table-row;">
+          <div style="display: table-cell;vertical-align: middle;text-align: center;">
+            <img src="http://img.guoguojia.vip/img/booking.png" style="width: 90px;"/>
+          </div>
+          <ul style="width:60%;display: table-cell;">
             <li>
               <div class="label" style="color: #866D8D;font-size: 16px;">
                 <i class="fa fa-shopping-cart" aria-hidden="true"></i>&nbsp;新鲜优质：
@@ -88,8 +95,10 @@ MsaView::registerJsFile($this,'/js/buy/type.js',
           </ul>
         </div>
         <!-- <hr/> -->
-        <div style="display: flex;flex-direction: row;justify-content: center;align-items: center;">
-            <img src="http://img.guoguojia.vip/img/sale.png" style="width: 90px;" /> 
+        <div style="display: table-row;">
+            <div style="display: table-cell;vertical-align: middle;text-align: center;">
+              <img src="http://img.guoguojia.vip/img/sale.png" style="width: 90px;" /> 
+            </div>
             <ul style="width:60%">
               <li>
                 <div class="label" style="font-size: 16px;">
@@ -113,12 +122,6 @@ MsaView::registerJsFile($this,'/js/buy/type.js',
       </div>
     </div>
 
-    <a class="btn btn-success" href="/buy/booking?id=<?=$id ?>" style="width: 50%;margin-bottom: 10px;margin-top:10px;">去预约</a>
-    <a class="btn btn-danger" href="/buy/?id=<?=$id ?>" style="width: 50%;">买现货</a>
-    <!-- <div style="display: -webkit-flex;flex-direction: -webkit-row;justify-content: -webkit-space-around;display: flex;flex-direction: row;justify-content: space-around;width:100%;margin-top:1%;">
-      <div class="choose_type" data-href='/buy/booking?id=<?=$id ?>' data-type=2 style="width:80px; height: 80px;border: 1px solid #866D8D;border-radius: 40px;background-color: #866D8D; margin-bottom: 1%;text-align: center;line-height: 80px;color:white;font-size: 16px;">爱预约</div>
-
-      <div class="choose_type" data-type=1 style="width:80px; height: 80px;border: 1px solid #53a93f;border-radius: 40px;background-color: #53a93f; margin-bottom: 1%;text-align: center;line-height: 80px;color:white;font-size: 16px;" data-href='/buy/?id=<?=$id ?>'>买现货</div>
-      </div>
-    </div> -->
+    <a class="btn btn-success" href="/buy/booking?id=<?=$id ?>" style="width: 50%;margin-left:25%;margin-top:10px;">去预约</a>
+    <a class="btn btn-danger" href="/buy/?id=<?=$id ?>" style="width: 50%;margin-left:25%;margin-top:10px;">买现货</a>
 </div>
