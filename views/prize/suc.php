c5f90b1b1d31958afd3afdfb82f5b507434f90e8
yangyuncai <yyc@yunlaba.com>
2018-01-23 02:50:01 +0800

    test share

--- views/prize/suc.php
+++ views/prize/suc.php
@@ -20,14 +20,15 @@ MsaView::registerJsFile($this,'/js/prize/index.js',
 
 
 <div style="display: table;width: 100%;height: 100%;">
-    <div style="display: table-cell;vertical-align: middle;text-align: center;">
+    <div style="display: table-row;vertical-align: middle;text-align: center;">
         <img src="/img/ggj.jpg" style="width: 200px;"/>
     </div>
-    <div style="display: table-cell;text-align: center;vertical-align: middle;">
-        <div>恭喜你</div>
+    <div style="display: table-row;text-align: center;vertical-align: middle;">
+        <div>恭喜你获得</div>
         <div><?=$text ?></div>
     </div>
-    <div style="display: table-cell;text-align: center;vertical-align: middle;">
+    <div style="display: table-row;text-align: center;vertical-align: middle;">
+        <div>长按识别二维码领奖</div>
         <img src="https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=<?=$ticket ?>" style="width:50%;" />
     </div>
 </div>
