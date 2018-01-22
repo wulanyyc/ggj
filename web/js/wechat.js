1f01c66ce2d4345211bc63ec37015df80f75ad65
yangyuncai <yyc@yunlaba.com>
2018-01-22 18:57:33 +0800

    test share

--- web/js/wechat.js
+++ web/js/wechat.js
@@ -13,15 +13,28 @@ if (document.getElementById('we_appid')) {
         //     menuList: ['onMenuShareQQ', 'onMenuShareQZone']
         // });
         // wx.hideAllNonBaseMenuItem();
-        // alert('ok');
-        // setCookie('wechat', 1, 1);
+
         wx.onMenuShareAppMessage({
-            title: '成都果果佳--开业钜惠',
+            title: 'test: 成都果果佳->开业钜惠, 好礼100%',
             desc: '全网最低价，新鲜水果、干果，保质保量。顺丰配送，当日或隔日达', // 分享描述
             link: 'http://guoguojia.vip/prize/?share_id=' + $.cookie('openid'), // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
             imgUrl: 'http://img.guoguojia.vip/img/ggj.jpg', // 分享图标
             type: 'link', // 分享类型,music、video或link，不填默认为link
             // dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+            success: function () {
+                // 用户确认分享后执行的回调函数
+                alert('ok');
+            },
+            cancel: function () {
+                // 用户取消分享后执行的回调函数
+                alert('fail');
+            }
+        });
+
+        wx.onMenuShareTimeline({
+            title: 'test: 成都果果佳->开业钜惠, 好礼100%', // 分享标题
+            link: 'http://guoguojia.vip/prize/?share_id=' + $.cookie('openid'), // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+            imgUrl: 'http://img.guoguojia.vip/img/ggj.jpg', // 分享图标
             success: function () {
                 // 用户确认分享后执行的回调函数
                 alert('ok');
