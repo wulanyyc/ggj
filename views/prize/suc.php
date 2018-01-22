d6198f474e300acb9e3432e9b18003e6f80a6ed6
yangyuncai <yyc@yunlaba.com>
2018-01-23 02:56:47 +0800

    test share

--- views/prize/suc.php
+++ views/prize/suc.php
@@ -21,11 +21,16 @@ MsaView::registerJsFile($this,'/js/prize/index.js',
 
 <div style="display: table;width: 100%;height: 100%;">
     <div style="display: table-row;vertical-align: middle;text-align: center;">
-        <img src="/img/ggj.jpg" style="width: 200px;"/>
+        <img src="/img/ggj.jpg" style="width: 30%;"/>
     </div>
     <div style="display: table-row;text-align: center;vertical-align: middle;">
-        <div>恭喜你获得</div>
-        <div><?=$text ?></div>
+        <div>恭喜您获得</div>
+        <div style="color:red;"><?=$text ?></div>
+    </div>
+
+    <div style="display: table-row;text-align: center;vertical-align: middle;">
+        <p>优惠券：在购买中直接减免订单金额</p>
+        <p style="color:red;">水果礼品：可在平台任何支付完成订单中领取，并随该订单一起发货</p>
     </div>
     <div style="display: table-row;text-align: center;vertical-align: middle;">
         <div>长按识别二维码领奖</div>
