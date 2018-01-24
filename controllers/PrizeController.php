6e6fbed98bb08f2e4c24084dd93f1be44324cdc8
yangyuncai <yyc@yunlaba.com>
2018-01-24 17:02:44 +0800

    adjust img

--- controllers/PrizeController.php
+++ controllers/PrizeController.php
@@ -22,7 +22,7 @@ class PrizeController extends Controller
         $sid = isset($params['share_id']) ? $params['share_id'] : '';
         $from = isset($params['from']) ? $params['from'] : 0;
 
-        if ($from === 'timeline' || $from === 'singlemessage' || !empty($_COOKIE['openid'])) {
+        // if ($from === 'timeline' || $from === 'singlemessage' || !empty($_COOKIE['openid'])) {
             if (empty($_COOKIE['puid'])) {
                 $uniq = uniqid();
                 setcookie('puid', $uniq, time() + 86400 * $this->dayLimit, '/');
@@ -37,11 +37,11 @@ class PrizeController extends Controller
             return $this->render('index', [
                 'controller' => Yii::$app->controller->id,
             ]);
-        }
+        // }
 
-        return $this->render('error', [
-            'controller' => Yii::$app->controller->id,
-        ]);
+        // return $this->render('error', [
+        //     'controller' => Yii::$app->controller->id,
+        // ]);
     }
 
     public function actionGetrotate() {
