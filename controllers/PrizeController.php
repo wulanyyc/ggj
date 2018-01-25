2f1609ab973c3b468f9f0207f6c8ae5ba481542a
yangyuncai <yyc@yunlaba.com>
2018-01-25 23:58:07 +0800

    adjust prize share

--- controllers/PrizeController.php
+++ controllers/PrizeController.php
@@ -126,17 +126,25 @@ class PrizeController extends Controller
 
         $cnt = Yii::$app->redis->get($cntKey);
 
+        $data = Yii::$app->redis->get($uniq . "_code");
+
         if ($cnt > $this->limit) {
             $remainTime = Yii::$app->redis->ttl($cntKey);
             $remainDay = round($remainTime / 86400, 1);
 
-            return $this->render('limit', [
+            $data = json_decode($data, true);
+            $ticket = $data['ticket'];
+            $prizeCode = $data['code'];
+
+            return $this->render('suc', [
                 'controller' => Yii::$app->controller->id,
-                'day' => $remainDay,
+                'ticket' => urlencode($ticket),
+                'text' => $prize['text'],
+                'code' => $prizeCode,
+                'day'  => $this->dayLimit,
+                'prizeLimit' => $this->prizeLimit,
             ]);
         }
-
-        $data = Yii::$app->redis->get($uniq . "_code");
 
         if (empty($data)) {
             $prizeCode = SiteHelper::getRandomStr(6);
