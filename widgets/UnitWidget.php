56a5fb5cf891c85a8ab8ee3bbed9c0dc2177647c
yangyuncai <yyc@yunlaba.com>
2018-02-07 18:56:51 +0800

    test coupon

--- widgets/UnitWidget.php
+++ widgets/UnitWidget.php
@@ -21,7 +21,7 @@ class UnitWidget extends Widget
      * 执行
      */
     public function run(){
-        $units = ['个','斤','盒', '份', '把', '周'];
+        $units = ['个','斤','盒', '份', '把', '周', '袋'];
 
         $html = '<select name="unit" class="unit" style="min-width:60px;">';
         foreach($units as $unit) {
